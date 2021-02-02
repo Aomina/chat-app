af941d478075d2ecfbb5c39ba6da416558b545b7
Aomina <minaduki-rider26@ezweb.ne.jp>
2021-02-02 13:34:47 +0900

    チャットルーム名の表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
